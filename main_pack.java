6c43f9b3fa59cd7055102afea45d855b5327e528
NIHAL814 <nihal89401@gmail.com>
2026-07-28 00:28:17 +0530

    Add files via upload

---
+++ main_pack.java
@@ -0,0 +1,11 @@
+package mypackage;
+import mypackage.student_pack;
+    public class main_pack{
+        public static void main(String[] args) {
+            student_pack s=new student_pack();
+            s.display();
+
+        }
+    }
+    
+
---
+++ student_pack.java
@@ -0,0 +1,10 @@
+package mypackage;
+public class student_pack{
+    void display(){
+        System.out.println("Name: Nihal");
+        System.out.println("Course: CS");
+
+    }
+}
+    
+
